7c68b4ba994c87554904ab9424b553b8a6f70bdd
Hellstern <tue@f1-support.dk>
2020-04-09 15:31:31 +0200

    Create COVID.R

---
+++ pitchme/code/COVID.R
@@ -0,0 +1,11 @@
+#
+# COVID-19
+#
+
+# Data
+# "https://www.ecdc.europa.eu/sites/default/files/documents/COVID-19-geographic-disbtribution-worldwide-2020-03-12.xls
+
+library(readxl)
+covid19 <- read_excel("COVID-19.xlsx")
+
+
