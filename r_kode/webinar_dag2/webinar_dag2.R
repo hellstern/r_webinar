219d24d34e1eb238a69ade82103ece08e3a7c95e
Hellstern <tue@f1-support.dk>
2020-04-10 09:51:50 +0200

    Create webinar_dag2.R

---
+++ r_kode/webinar_dag2/webinar_dag2.R
@@ -0,0 +1,17 @@
+# *************************** 
+# Webinar
+# Dag 2
+# Tue Hellstern
+# *************************** 
+
+# ***************************
+# Working Directory
+# *************************** 
+getwd()
+# setwd("C:/Users/Tue Hellstern/Documents/GitHub/r_webinar/r_kode/webinar_dag2")
+
+# ***************************
+# Pakker
+# *************************** 
+install.packages("dplyr")  
+library(dplyr)
